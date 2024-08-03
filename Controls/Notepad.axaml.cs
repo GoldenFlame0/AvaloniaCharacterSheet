0eb44bcdb70c2b4b727abf2816ecce2ae743bbeb
BuildTools <goldenflamezero@gmail.com>
2024-08-03 22:18:49 +1000

    Initial Commit

---
+++ Controls/Notepad.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CharacterSheet.Controls;
+
+public partial class Notepad : UserControl
+{
+    public Notepad()
+    {
+        InitializeComponent();
+    }
+}
